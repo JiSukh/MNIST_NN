Fix numerous counts of improper gradient descent, added clips to prevent gradient explosion and overflow, finished training model - Neural network now works!

ActivationSoftMax.py
@@ -6,8 +6,8 @@ class ActivationSoftMax:
     Activate a stable soft max algorithm to an entire layer
     """
     def forward(self, inputs):
-        z = inputs - max(inputs)
+        z = inputs - np.max(inputs, axis=-1, keepdims=True)
         numerator = np.exp(z)
-        denominator = np.sum(numerator)
+        denominator = np.sum(numerator, axis=-1, keepdims=True)
 
         self.output = numerator/denominator

DenseLayer.py
@@ -30,5 +30,8 @@ class DenseLayer:
     def backward(self, delta_outputs, learning_rate):
         """Backwards prop. for dense layer
         """
-        self.weights = -learning_rate * np.outer(self.inputs, delta_outputs)
-        self.biases = -learning_rate * delta_outputs
+        self.weights += -learning_rate * np.clip(np.outer(self.inputs, delta_outputs),-1,1)
+        self.biases += -learning_rate * np.clip(delta_outputs,-1,1)
+        
+        self.delta_output = np.dot(delta_outputs, self.weights.T)
+        